3f7ca9e1ad178bea2b0d2d83c3f433879488c675
Yewei <yeweichen1@gmail.com>
2022-03-05 22:06:57 -0600

    add validatetions

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,4 +1,7 @@
 class Organisation < ApplicationRecord
     has_many :users
     has_many :shifts, through: :users
+
+    validates :name, presence: true
+    validates :hourly_rate, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :shifts
   belongs_to :organisation
   validates :name, uniqueness: true, presence: true
-  validates :password_digest, presence: true, length:{minimum: 6, message: "minimum 6 characters"}
+  validates :password_digest, presence: true, length:{minimum: 6, message: "minimum 6 characters"}, conformation: true
   validates :email_address, presence: true
 end
